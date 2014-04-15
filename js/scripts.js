f597daaa80d44182128b75e286b3760c924d41a5
josephbergdoll <joseph@osephj.com>
2014-04-15 16:57:38 -0400

    proper transitions

--- js/scripts.js
+++ js/scripts.js
@@ -9,13 +9,13 @@ $(document).ready(function(){
     $('.main-content').waypoint(function(direction) {
     $('.js-hero').toggleClass('fade-hero', direction === 'down');
   }, {
-    offset: '70%'
+    offset: '95%'
   });
 
     $('.main-content').waypoint(function(){
       $(this).toggleClass('fade-contents');
     }, {
-      offset: '70%'
+      offset: '95%'
     });
 
   $('.headlines h1').fitText(1.0, { minFontSize: '24px', maxFontSize: '96px' });
@@ -27,6 +27,10 @@ $(document).ready(function(){
 $(window).load(function(){
 
   $('.headlines').delay(1000).queue(function(){
+    $(this).removeClass('fade-contents');
+  });
+
+  $('.logo').delay(1500).queue(function(){
     $(this).removeClass('fade-contents');
   });
 
